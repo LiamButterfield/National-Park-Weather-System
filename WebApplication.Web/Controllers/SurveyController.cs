9236372aaad29af9e2ce8551a0dbc52a47b9fce4
Becki Robards <becki.robards@gmail.com>
2019-03-13 16:52:12 -0400

    Added Survey controller, Survey model, and Index view

---
+++ WebApplication.Web/Controllers/SurveyController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Web.DAL;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Web.Controllers
+{
+    public class SurveyController : Controller
+    {
+        private ISurveyDAO surveyDao;
+        public SurveyController(ISurveyDAO surveyDao)
+        {
+            this.surveyDao = surveyDao;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                surveyDao.CreateSurvey(survey);
+                TempData["PostSuccessful"] = "Your post was successful!";
+                return RedirectToAction("FavoritePark");
+            }
+            else
+            {
+                return View(survey);
+            }
+        }
+
+        public IActionResult FavoritePark()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication.Web/DAL/SurveySqlDAO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Web.DAL
+{
+    public class SurveySqlDAO : ISurveyDAO
+    {
+        private string connectionString { get; set; }
+        public SurveySqlDAO(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public bool CreateSurvey(Survey survey)
+        {
+            bool saved = false;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO survey_result VALUES(@parkCode, @emailAddress, @state, @activityLevel);", conn);
+                    cmd.Parameters.AddWithValue("@parkCode", survey.ParkCode);
+                    cmd.Parameters.AddWithValue("@emailAddress", survey.EmailAddress);
+                    cmd.Parameters.AddWithValue("@state", survey.State);
+                    cmd.Parameters.AddWithValue("@activityLevel", survey.ActivityLevel);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException ex)
+            {
+
+                throw;
+            }
+
+            return saved;
+        }
+    }
+}
