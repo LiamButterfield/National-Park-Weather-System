f5cb6fca5d5c8b4a140e638bd4e407f6e1e43d1b
Becki Robards <becki.robards@gmail.com>
2019-03-13 09:49:30 -0400

    Created Park model & DAOs; added index view for all parks

--- WebApplication.Web/Controllers/HomeController.cs
+++ WebApplication.Web/Controllers/HomeController.cs
@@ -4,15 +4,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Web.DAL;
 using WebApplication.Web.Models;
 
 namespace WebApplication.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IParkDAO parkDao;
+        public HomeController(IParkDAO parkDao)
+        {
+            this.parkDao = parkDao;
+        }
+
+        [HttpGet]
         public IActionResult Index()
-        {            
-            return View();
+        {
+            IList<Park> model = parkDao.GetAllParks();
+            return View(model);
         }       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ WebApplication.Web/DAL/ParkSqlDAO.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Web.DAL
+{
+    public class ParkSqlDAO : IParkDAO
+    {
+        private string connectionString { get; set; }
+        public ParkSqlDAO (string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public IList<Park> GetAllParks()
+        {
+            IList<Park> parks = new List<Park>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand ("SELECT * FROM park;", conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Park park = ConvertReaderToPark(reader);
+                        parks.Add(park);
+                    }
+                }
+            } 
+            catch(SqlException ex)
+            {
+                throw;
+            }
+            return parks;
+        }
+
+        private Park ConvertReaderToPark(SqlDataReader reader)
+        {
+            Park park = new Park();
+
+            park.ParkCode = Convert.ToString(reader["parkCode"]);
+            park.ParkName = Convert.ToString(reader["parkName"]);
+            park.State = Convert.ToString(reader["state"]);
+            park.Acreage = Convert.ToInt32(reader["acreage"]);
+            park.ElevationInFeet = Convert.ToInt32(reader["elevationInFeet"]);
+            park.MilesOfTrail = Convert.ToDecimal(reader["milesOfTrail"]);
+            park.NumberOfCampsites = Convert.ToInt32(reader["numberOfCampsites"]);
+            park.Climate = Convert.ToString(reader["climate"]);
+            park.YearFounded = Convert.ToInt32(reader["yearFounded"]);
+            park.AnnualVisitorCount = Convert.ToInt32(reader["annualVisitorCount"]);
+            park.Quote = Convert.ToString(reader["inspirationalQuote"]);
+            park.QuoteSource = Convert.ToString(reader["inspirationalQuoteSource"]);
+            park.ParkDescription = Convert.ToString(reader["parkDescription"]);
+            park.EntryFee = Convert.ToInt32(reader["entryFee"]);
+            park.AnimalSpecies = Convert.ToInt32(reader["numberOfAnimalSpecies"]);
+
+            return park;
+        }
+    }
+}
